19c412758e123aa750c638557c3c26539eeecffc
lunar-alt <suryakiran.andoor2022@vitstudent.ac.in>
2024-06-08 11:31:39 +0530

    log and intent (dialer and web)

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.login
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-clickHandler","Button Clicked")
+        var dialIntent: Intent=Intent(Intent.ACTION_DIAL, Uri.parse("tel:9847531304"))
+        var webIntent:Intent=Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+        startActivity(webIntent)
     }
 }
