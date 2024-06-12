34fab5332f77fd7442ca28c0f873a0a515b0d3c1
lunar-alt <suryakiran.andoor2022@vitstudent.ac.in>
2024-06-12 11:14:59 +0530

    logcat msg and exception handling

---
+++ app/src/main/java/com/example/login/HomeActivity.kt
@@ -0,0 +1,30 @@
+package com.example.login
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //get intent which sttarted this activity
+        //get the extras from that intent
+        //get the string with mykey
+        var data = intent.extras?.getString("mykey")
+        Log.i("homeactivity",data.toString())
+        //put the data either in a log or on the textview
+        var homeTextView:TextView = findViewById(R.id.tvHome)
+        homeTextView.setText(data)
+    }
+}
--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,24 +1,82 @@
 package com.example.login
 
+
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.login.kotlinjavaex.Employee
 
 class MainActivity : AppCompatActivity() {
+    //activiy is getting created -- chick is in the egg -- mem being allocated for activity
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //inflate this xml
         setContentView(R.layout.activity_main)
+        Log.i(TAG,"activity is getting created")
+        // inflateXml()
+        // var myEmp = Employee("abdul")
+    }
+
+    //chick came out of the egg --activity is visible to the user to interact - click
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"activity is started")
+    }
+
+    //sleep night -- incomming call ur activity goes into the background for sometime
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG,"activity has paused")
+
+    }
+
+
+    //chick has woken up -- activity comes back to the foreground
+    override fun onResume() {
+        super.onResume()
+        Log.w(TAG, "activity has resumed")
+    }
+
+    //death of the chick -- activity no lonnger exists in the ram -- pushed to hdd[hibernate]
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"activity is stopped")
+
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG,"activity is destroyed")
     }
 
     fun clickHandler(view: View) {
-        Log.i("MainActivity-clickHandler","Button Clicked")
-        var dialIntent: Intent=Intent(Intent.ACTION_DIAL, Uri.parse("tel:9847531304"))
-        var webIntent:Intent=Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
-        startActivity(webIntent)
+        Log.i("MainActivity-clickhandler","button clicked")
+        /*  var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+           var webIntent: Intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://www.ndtv.com"))
+           startActivity(webIntent)*/
 
+        var hIntent:Intent = Intent(this,HomeActivity::class.java)
+        hIntent.putExtra("mykey","android-vit-abdul")
+        //throw NullPointerException("demo vit exception")
+        startActivity(hIntent)
     }
+
+
+    fun inflateXml(){
+        var nameEditText = EditText(this)
+        nameEditText.setHint("enter ur name")
+        var pwdEditText = EditText(this)
+        pwdEditText.setHint("enter ur pass")
+        var loginButton = Button(this)
+        loginButton.setText("login")
+    }
+
 }
